91c42745a495e94e0ead6fae9f4e40334d8a3101
ahmdmarzuki <ahmadmarzuki303@gmail.com>
2025-10-13 13:33:32 +0700

    refactor: layout

--- src/app/test-contract/page.tsx
+++ src/app/test-contract/page.tsx
@@ -5,9 +5,10 @@ import React from "react";
 
 const page = () => {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-950 text-white">
-      <h1 className="text-3xl font-bold mb-4">🎟️ NFT Ticketing App</h1>
-      <ConnectButton />
+    <main className="flex flex-col items-center justify-center py-10 bg-gray-950 text-white">
+      <div className="">
+        <ConnectButton />
+      </div>
     </main>
   );
 };
