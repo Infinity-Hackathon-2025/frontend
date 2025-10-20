e31cec372d886d5a723b4f788980072d1254d1be
anastaciuh <anastaciaputri05@gmail.com>
2025-10-21 03:56:21 +0700

    fix: about-page

--- src/app/landingpage/components/about.tsx
+++ src/app/landingpage/components/about.tsx
@@ -3,25 +3,22 @@ import React from "react";
 
 export default function AboutSection() {
   return (
-<div className="pt-28 px-6 md:px-10 max-w-7xl mx-auto space-y-10">
-  <section className="grid grid-cols-1 md:grid-cols-2 gap-10">
+<div className="pt-0 px-6 md:px-10 max-w-7xl mx-auto space-y-10">
+  <section className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
     <div className="flex justify-center items-center">
       <img
         src="/images/asset2.png" 
         alt="About"
-        className="w-full md:w-[2000px] max-w-full object-cover rounded-lg" 
+        className="w-full md:w-[1000px] md:px-0.5 lg:w-[1100px] xl:w-[1200px] max-w-none object-cover rounded-lg -ml-4" 
       />
     </div>
 
         <div className="space-y-6 text-right"> 
           <h1 className="text-4xl font-extrabold text-[#1A3DA7]">
-            Lorem Ipsum Dolor Sit Tagline 2
+            ABOUT US
           </h1>
           <p className="text-xl text-gray-700">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+          <b>TRUSTIX</b> adalah platform tiket digital berbasis teknologi terdesentralisasi yang dirancang untuk menciptakan pengalaman pembelian tiket event yang aman, transparan, dan bebas calo. Kami percaya, setiap orang berhak mendapatkan akses ke acara favorit mereka tanpa khawatir akan tiket palsu atau sistem pembelian yang rumit.
           </p>
         </div>
       </section>
