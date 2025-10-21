d36a36cc51ce9cb08dd841290170b9ee7ea1442b
luvinscha <luvinscha.dinasty@gmail.com>
2025-10-21 16:19:11 +0700

    refactor: landingpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
-import LandingPage from "./landingpage/page";
+import Hero from "./components/hero";
+import About from "./components/about";
+import ConcertHighlight from "./components/concert-highlight";
+import TrendingConcerts from "./components/trending-concerts";
+import BestsellingMerch from "./components/bestselling-merch";
+import Resells from "./components/resells";
+import HelpCenter from "./components/help-center";
+import FAQ from "./components/faq";
 
-export default LandingPage;
+export default function HomePage() {
+  return (
+    <div className="space-y-24 pb-20">
+      <Hero />
+      <About />
+      <ConcertHighlight />
+      <TrendingConcerts />
+      <BestsellingMerch />
+      <Resells />
+      <HelpCenter />
+      <FAQ />
+    </div>
+  );
+}
