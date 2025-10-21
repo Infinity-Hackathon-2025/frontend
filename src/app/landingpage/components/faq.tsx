581028b219635a4d47eca180f8669868a56f32ea
anastaciuh <anastaciaputri05@gmail.com>
2025-10-21 07:39:55 +0700

    fix: help center dan faq

--- src/app/landingpage/components/faq.tsx
+++ src/app/landingpage/components/faq.tsx
@@ -16,28 +16,28 @@ export default function FAQ() {
               {faqs.map((text, i) => (
                 <div
                   key={i}
-                  className="flex items-center justify-between py-3 px-4 text-sm text-[#ffffff]/80 hover:bg-[#E3F2FD] cursor-pointer border-b border-solid border-[#ffffff]"
+                  className="flex items-center justify-between py-3 px-4 text-sm text-gray-500 hover:bg-[#E3F2FD] cursor-pointer border-b border-solid border-gray-500"
                 >
                   {text}
-                  <span className="text-[#ffffff]">›</span>
+                  <span className="text-[#0038BD] text-xl font-bold">›</span>
                 </div>
               ))}
             </div>
   
             <a
               href="#"
-              className="block mt-3 text-sm font-medium underline text-[#1F2B6C] hover:text-[#2F54EB]"
+              className="block mt-7 text-sm font-medium underline text-[#0038BD] hover:text-[#3F78FF]"
             >
               Lihat lainnya
             </a>
           </div>
   
-          <div className="rounded-2xl bg-white/60 backdrop-blur-lg p-4 shadow border border-white/40 w-[400px]">
-            <h4 className="font-[Nexa] text-[#1F2B6C] text-lg font-semibold">Masih Bingung?</h4>
-            <p className="text-sm text-[#171717]/70 mt-2 leading-relaxed">
+          <div className="rounded-2xl bg-white/60 backdrop-blur-lg p-4 shadow border border-white/40 w-[400px] h-[220px]">
+            <h4 className="font-[Nexa] text-[#1F2B6C] text-3xl font-semibold">Masih Bingung?</h4>
+            <p className="text-sm text-[#171717]/70 mt-5 leading-relaxed">
               Butuh bantuan lebih lanjut? Ketik di bawah untuk hubungi customer service kami.
             </p>
-            <button className="mt-3 rounded-full bg-[#153E9C] text-white text-sm px-4 py-2 shadow hover:bg-[#2F54EB] transition">
+            <button className="mt-10 rounded-full bg-[#153E9C] text-white text-sm px-4 py-2 shadow hover:bg-[#2F54EB] transition">
               Hubungi Kami
             </button>
           </div>
--- src/app/landingpage/components/help-center.tsx
+++ src/app/landingpage/components/help-center.tsx
@@ -1,16 +1,16 @@
 export default function HelpCenter() {
     return (
       <section className="mx-auto max-w-6xl px-4">
-        <h3 className="font-[Mont] text-2xl text-[#1F2B6C] font-semibold mb-4">Help Center</h3>
+        <h3 className="font-[Mont] text-3xl text-[#0038BD] font-semibold mb-4">Help Center</h3>
   
         <div className="relative">
           <input
             type="text"
             placeholder="Ketik topik yang Anda ingin cari (misal: Refund)"
-            className="w-full rounded-full border border-white/60 bg-white/60 backdrop-blur px-5 py-2 text-sm shadow focus:outline-none"
+            className="w-full rounded-full border border-white/60 bg-white/60 backdrop-blur px-5 py-2 text-sm text-black shadow focus:outline-none"
           />
-          <button className="absolute right-3 top-1/2 -translate-y-1/2 text-[#1F2B6C]">
-            🔍
+          <button className="absolute right-0 top-1/2 -translate-y-1/2">
+            <img src="/images/search.png" alt="Search" className="w-13 h-13" />
           </button>
         </div>
       </section>
